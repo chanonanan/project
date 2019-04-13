972f80c9d55d90f56f39a99586f2d8c9b1ec42e7
chanonanan <chanonanan@gmail.com>
2019-04-13 19:53:47 +0700

    Record Model

--- models/record.js
+++ models/record.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Record = sequelize.define('Record', {
-    timestamp: DataTypes.DATE
+    duration: DataTypes.INTEGER,
+    from: DataTypes.STRING,
+    to: DataTypes.STRING
   }, {});
   Record.associate = function(models) {
     // associations can be defined here
