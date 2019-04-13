c1d9b8baedd83b271e29ec8e1b7e5f5fa53cb49b
chanonanan <chanonanan@gmail.com>
2019-04-13 19:52:50 +0700

    rec

--- app/web/record.js
+++ app/web/record.js
@@ -2,12 +2,19 @@ var models = require('../../models');
 const Op = require('sequelize').Op;
 module.exports = {
     store: (rec) => {
-        console.log("store", rec)
+        var duration = rec.duration;
+        var from = rec.from;
+        var to = rec.to;
+        var test_id = rec.test_id;
+        console.log("duration", duration)
+        console.log("from", from)
+        console.log("to", to)
+        console.log("test_id", test_id)
         models.Record.create({
-            duration: rec.duration,
-            from: rec.from,
-            to: rec.to,
-            test_id: rec.test_id,
+            duration: duration,
+            from: from,
+            to: to,
+            test_id: test_id,
         }).then(res => {
             console.log("create success",res);
         })
