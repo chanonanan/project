1b9774c24719dab36426f5a66b965127c754fa2a
chanonanan <chanonanan@gmail.com>
2019-04-14 10:47:53 +0700

    datatype

--- models/record.js
+++ models/record.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       primaryKey: true,
       type: DataTypes.UUID,
-      defaultValue: Sequelize.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
       validate: {
         notNull: true
       }
